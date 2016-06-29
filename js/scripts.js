287ce2bc5c32182adbbe582e5e744518cb90f6ab
Russvetsper <russvetsper@gmail.com>
2016-06-28 22:23:58 -0700

    fix indentations in js

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,36 @@ $(document).ready(function() {
     var test = $("input#number").val();
     var test2 = parseInt(test);
     var list = "";
+
       for (var i = 1; i <= test2; i ++) {
       list += "<li>";
+
+
       if ((i % 3 === 0) && (i % 5 === 0)){
         list += "<i>ping-pong</i>"
       }
+
+
       else if ((i % 3 === 0) && (i % 5 !== 0)){
         list += "<i>ping</i>";
          }
+
+
       else if ((i % 5 === 0) && (i % 3 !== 0)){
         list += "<i>pong</i>"
 
       }else{
+
         list += i;
+
       }
+
           list += "</li>";
+
       };
-            $("#results").append(list);
+
+
+      $("#results").append(list);
+
   });
 });
