1ab60b70c3a4012924fd8956f65b3a609799897e
Russvetsper <russvetsper@gmail.com>
2016-06-28 22:15:22 -0700

    add business interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
+  $("form#form-group").submit(function(event) {
+    event.preventDefault();
 
 
+    var test = $("input#number").val();
+    var test2 = parseInt(test);
+    var list = "";
+      for (var i = 1; i <= test2; i ++) {
+      list += "<li>";
+      if ((i % 3 === 0) && (i % 5 === 0)){
+        list += "<i>ping-pong</i>"
+      }
+      else if ((i % 3 === 0) && (i % 5 !== 0)){
+        list += "<i>ping</i>";
+         }
+      else if ((i % 5 === 0) && (i % 3 !== 0)){
+        list += "<i>pong</i>"
 
-
-event.preventDefault();
+      }else{
+        list += i;
+      }
+          list += "</li>";
+      };
+            $("#results").append(list);
+  });
 });
