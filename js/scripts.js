8d88a5d865d8590130675e1808e1d61d70baaa94
russ <russvetsper@gmail.com>
2016-06-29 16:11:46 -0700

    change tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,22 @@
 $(document).ready(function() {
   $("form#form-group").submit(function(event) {
     event.preventDefault();
-
-
     var input = $("input#number").val();
     var input2 = parseInt(input);
     var list = "";
 
-      for (var i = 1; i <= input2; i ++) {
-        list += "<li>";
+   for (var i = 1; i <= input2; i ++) {
+    if ((i % 3 === 0) && (i % 5 === 0)){
+      list += "<li>"+i+"ping-pong</li>"
+    } else if ((i % 3 === 0) && (i % 5 !== 0)){
+      list += "<li>"+i+"ping</li>";
+    } else if ((i % 5 === 0) && (i % 3 !== 0)){
+      list += "<li>"+i+"pong</li>"
+    } else {
+      list+="<li>"+i+"</li>"
+    }
+    };
 
-
-      if ((i % 3 === 0) && (i % 5 === 0)){
-        list += "<li>ping-pong</li>"
-      }
-
-
-      else if ((i % 3 === 0) && (i % 5 !== 0)){
-        list += "<li>ping</li>";
-
-      }
-
-      else if ((i % 5 === 0) && (i % 3 !== 0)){
-        list += "<li>pong</li>"
-
-      }else{
-
-        list += i;
-
-      }
-
-        list += "</li>";
-
-      };
-
-
-      $("#results").append(list);
-
+    $("#results").append(list);
   });
 });
